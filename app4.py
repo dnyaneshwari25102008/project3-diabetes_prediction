db89873587bdfc67abe1a94e9c0622a331bddc07
dnyaneshwari25102008 <bhosalednyaneshwari36@gmail.com>
2026-07-02 21:52:22 +0530

    Add files via upload

---
+++ app4.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import pickle
+st.title("    🩺 Diabetes Prediction App")
+#st.write(" ")
+
+st.image("dia.jpg")
+df=pd.read_csv("final.csv")
+
+
+model = pickle.load(open("model.pkl", "rb"))
+
+
+
+st.markdown("""
+<style>
+.stApp {
+    background-color:#E8F2FD;
+}
+</style>
+""", unsafe_allow_html=True)
+
+pregnancies = st.sidebar.number_input("Pregnancies", min_value=0)
+glucose = st.sidebar.number_input("Glucose", min_value=0)
+bloodpressure = st.sidebar.number_input("Blood Pressure", min_value=0)
+skinthickness = st.sidebar.number_input("Skin Thickness", min_value=0)
+insulin = st.sidebar.number_input("Insulin", min_value=0)
+bmi = st.sidebar.number_input("BMI", min_value=0.0)
+dpf = st.sidebar.number_input("Diabetes Pedigree Function", min_value=0.0)
+age = st.sidebar.number_input("Age", min_value=0)
+
+if st.sidebar.button("Predict"):
+
+    myinput = [[pregnancies, glucose, bloodpressure,
+                skinthickness, insulin, bmi,
+                dpf, age]]
+
+    columns = [
+        'Pregnancies',
+        'Glucose',
+        'BloodPressure',
+        'SkinThickness',
+        'Insulin',
+        'BMI',
+        'DiabetesPedigreeFunction',
+        'Age'
+    ]
+
+    myinput = pd.DataFrame(myinput, columns=columns)
+
+    result = model.predict(myinput)
+    st.subheader("Prediction Result")
+
+    if result[0] == 1:
+        st.error("⚠️ Diabetes Detected")
+    else:
+        st.success("✅ No Diabetes Detected")
